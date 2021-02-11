5d8c47fdff9b4f41d23ee001b8d48903a6938875
wgoops wgoops <wgoops@gmail.com>
2021-02-11 09:27:27 -0500

    welp

---
+++ repeat_string.js
@@ -0,0 +1,10 @@
+function repeating_string(string, repeat) { 
+    concatenated_string = string
+    for (i = 0; i < repeat; i++){
+        concatenated_string += string 
+    }
+
+    return concatenated_string 
+}
+
+console.log(repeating_string("shaine", 1000))
